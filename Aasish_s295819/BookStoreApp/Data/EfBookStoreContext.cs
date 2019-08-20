423f0378a3c4637054a17ae4ffd20c663a2f59e7
Aasish Adhikari <gemini.aasis@gmail.com>
2019-08-20 23:35:25 +0930

    Creating Context

---
+++ Aasish_s295819/BookStoreApp/Data/EfBookStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using BookStoreApp.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Data
+{
+    public class EfBookStoreContext: DbContext
+    {
+        public EfBookStoreContext (DbContextOptions<EfBookStoreContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Printer> Printer { get; set; }
+    }
+}
