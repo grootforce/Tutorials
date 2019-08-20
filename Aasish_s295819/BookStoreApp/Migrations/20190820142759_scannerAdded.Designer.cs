dd24179ebc209e31cf1b2239d26f0a4ba2978694
Aasish Adhikari <gemini.aasis@gmail.com>
2019-08-20 23:58:39 +0930

    Database migration and update after scanner class added

--- Aasish_s295819/BookStoreApp/Migrations/20190820142759_scannerAdded.Designer.cs
+++ Aasish_s295819/BookStoreApp/Migrations/20190820142759_scannerAdded.Designer.cs
@@ -3,14 +3,16 @@ using BookStoreApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStoreApp.Migrations
 {
     [DbContext(typeof(EfBookStoreContext))]
-    partial class EfBookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20190820142759_scannerAdded")]
+    partial class scannerAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
