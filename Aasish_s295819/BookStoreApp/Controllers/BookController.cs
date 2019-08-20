6672673665dc576abcaa9be1cf2b8b9c24220415
Aasish Adhikari <gemini.aasis@gmail.com>
2019-08-21 01:16:41 +0930

    CRUD for book controller Basic crud action has been created for the book controller. The logics has not been added yet. Also the view does not contain any frontend for display of data.

---
+++ Aasish_s295819/BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
