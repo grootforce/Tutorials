b7dba1f34a2748ad8b5cdc868d06e5882ae9843a
jbenesha <joel.b13@hotmail.com>
2019-08-21 12:55:43 +0930

    scanner and printer

--- Joel/BookStoreApp/BookStoreApp/DAL/Scanner.cs
+++ Joel/BookStoreApp/BookStoreApp/DAL/Scanner.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.DAL
 {
-    public class Printer
+    public class Scanner
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public string  Brand { get; set; }
+        public string Brand { get; set; }
         public string Model { get; set; }
+
     }
 }
