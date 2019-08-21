32f48d87c9b0081d71686b1ae45463b8649513f5
jbenesha <joel.b13@hotmail.com>
2019-08-21 10:24:14 +0930

    mvc movie

---
+++ Joel/WebApplication1_week3/WebApplication1_week3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_week3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
